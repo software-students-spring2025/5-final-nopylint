3b7d7f67b8fb268a873a817ea6fccc10105f8f5c
nina-jsl <jl13999@nyu.edu>
2025-04-20 15:57:44 -0400

    sample test data

--- web_app/web/app.py
+++ web_app/web/app.py
@@ -1,6 +1,15 @@
-from flask import render_template
+import os
+import json
+from flask import render_template, jsonify
 
 def register_routes(app):
     @app.route('/')
     def index():
         return render_template('index.html')
+
+    @app.route('/api/weather')
+    def weather_api():
+        json_path = os.path.join(os.path.dirname(__file__), '..', 'api', 'weather.json')
+        with open(json_path, 'r') as file:
+            data = json.load(file)
+        return jsonify(data)
